331ab13c1ebec79b1a2f34a470a494aaacd29d41
Talha Hussain <talh768@gmail.com>
2022-07-26 00:46:24 -0400

    bruh

---
+++ examples/bruh.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int i;
+	cout << "enter a number to square: " << endl;
+	cin >> i;
+	cout << "squared number: " << (i * i) << endl;
+	return 0;
+}

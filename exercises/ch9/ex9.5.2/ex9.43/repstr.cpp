00731e54ef7e4c6fd5c6e5a12d13189663d35bf2
Talha Hussain <talh768@gmail.com>
2022-07-27 00:11:24 -0400

    tried to fix

--- exercises/ch9/ex9.5.2/ex9.43/repstr.cpp
+++ exercises/ch9/ex9.5.2/ex9.43/repstr.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <string>
 
+using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
@@ -13,21 +14,21 @@ using std::string;
 void val_rep(string &s, string &oldval, string &newval) {
 	if (s.empty() || oldval.empty()|| s.size() < oldval.size()) return;
 	cout << "old string: " << s << endl;
-
 	auto iter = s.begin();
 	int oldsiz = oldval.size();
 	int newsiz = newval.size();
 	string str(s.begin(), s.begin() + oldsiz);
-	while (iter != s.end()) {
+	while (iter + 1 != s.end()) {
 		if (str == oldval) {
-			iter = s.erase(iter, oldsiz);
-			iter = (s.insert(iter, newval)) + newsiz;
-			continue;
+			iter = s.erase(iter - 1, iter + oldsiz - 1);
+			iter = (s.insert(iter, newval.begin(), newval.end())) + newsiz;
 		} else {
-			str.erase(str.begin(), 1);
-			str.append(*iter);
+			str.erase(str.begin());
+			str.append(iter, iter + 1);
 			++iter;
 		}
+		str.erase(str.begin());
+		str.append(iter, iter + 1);
 	}
 	cout << "new string: " << s << endl;
 }
@@ -36,14 +37,20 @@ void val_rep(string &s, string &oldval, string &newval) {
 int main()
 {
 	string line, rep, newval;
-	cout << "enter string to replace: ";
-	cin >> rep;
-	cout << endl;
-	cout << "enter string to be inserted: ";
-	cin >> newval;
-	cout << endl;
 	cout << "enter line to be replaced: ";
-	getline(cin, line);
-	val_rep(line, rep, newval);
+	while (getline(cin, line, '\n')){
+		if (line.empty()){
+			continue;
+		}
+		cout << "enter string to replace: ";
+		cin >> rep;
+		cout << endl;
+		cout << "enter string to be inserted: ";
+		cin >> newval;
+		cout << endl;
+		val_rep(line, rep, newval);
+		cout << "enter line to be replaced: ";
+
+	}
 	return 0;
 }

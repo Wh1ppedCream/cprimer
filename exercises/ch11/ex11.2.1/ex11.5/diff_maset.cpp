e07d38e4c2c8f7b6025ee9115107c2667e3416d9
Talha Hussain <talh768@gmail.com>
2022-08-04 23:15:27 -0400

    stuff

---
+++ exercises/ch11/ex11.2.1/ex11.5/diff_maset.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <set>
+#include <map>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	std::set<int> sint;
+	//set only contains a key, so its used in storing singular values
+	std::map<int, int> mint;
+	//map contians both a key and an associated value, so it is used when you need two seperate values
+	// which associate with each other
+}
+

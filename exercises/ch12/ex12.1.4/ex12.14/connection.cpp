23343f548457b8b09a47d7097f9aa466d733f97e
Talha Hussain <talh768@gmail.com>
2022-08-26 20:13:23 -0400

    finished ex12.1.4

---
+++ exercises/ch12/ex12.1.4/ex12.14/connection.cpp
@@ -0,0 +1,61 @@
+#include <memory>
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::make_shared;
+using std::shared_ptr;
+using std::cin;
+using std::cout;
+using std::endl;
+
+struct destination
+{
+	string address;
+	int active_con = 0;
+	destination() = default;
+	destination(const string &st) : address(st) { };
+};
+
+struct connection 
+{
+	string address;
+	int port;
+	connection() = default;
+	connection(const string &st, int num) : address(st), port(num) { };
+};
+
+
+connection connect(destination *dst) { // open the connection
+	return connection(dst->address, dst->active_con);
+}
+
+
+void disconnect(connection &con) {	// close the given connection
+	(con.address).clear();
+	con.port = 0;
+}
+
+void end_connection(connection *p) {
+	disconnect(*p);
+}
+
+void f(destination &d /* other parameters */) {
+	d.active_con += 1;
+	connection c = connect(&d);
+
+	shared_ptr<connection> p(&c, end_connection);
+	cout << "connection number: " << p->port << endl;
+	d.active_con -= 1;
+}
+
+
+int main()
+{
+	auto d = destination("Talha's wifi");
+	f(d);
+	return 0;
+
+
+}
+
